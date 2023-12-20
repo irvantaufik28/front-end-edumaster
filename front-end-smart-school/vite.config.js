084a878d94cc03a9432460b70b2c4256ced1aa8f
irvantaufik <irvantaufik28@gmail.com>
2023-12-20 12:06:26 +0700

    vite config test

--- front-end-smart-school/vite.config.js
+++ front-end-smart-school/vite.config.js
@@ -1,29 +1,37 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// // https://vitejs.dev/config/
+// export default defineConfig ({
+//   build: {
+//     manifest: true, // Generate manifest.json
+//     rollupOptions: {
+//       external: [
+//         '/public/plugins/jquery/jquery.min.js',
+//         '/public/plugins/jquery-ui/jquery-ui.min.js',
+//         '/public/plugins/bootstrap/js/bootstrap.bundle.min.js',
+//         '/public/plugins/chart.js/Chart.min.js',
+//         '/public/plugins/sparklines/sparkline.js',
+//         '/public/plugins/jqvmap/jquery.vmap.min.js',
+//         '/public/plugins/jqvmap/maps/jquery.vmap.usa.js',
+//         '/public/plugins/jquery-knob/jquery.knob.min.js',
+//         '/public/plugins/moment/moment.min.js',
+//         '/public/plugins/daterangepicker/daterangepicker.js',
+//         '/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+//         '/public/plugins/summernote/summernote-bs4.min.js',
+//         '/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+//         '/public/dist/js/adminlte.js',
+//         '/public/dist/js/pages/dashboard.js',
+//         '/public/dist/css/adminlte.min.css'
+//       ],
+//     },
+//   },
+//   plugins: [react()],
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
-export default defineConfig ({
-  build: {
-    manifest: true, // Generate manifest.json
-    rollupOptions: {
-      external: [
-        '/public/plugins/jquery/jquery.min.js',
-        '/public/plugins/jquery-ui/jquery-ui.min.js',
-        '/public/plugins/bootstrap/js/bootstrap.bundle.min.js',
-        '/public/plugins/chart.js/Chart.min.js',
-        '/public/plugins/sparklines/sparkline.js',
-        '/public/plugins/jqvmap/jquery.vmap.min.js',
-        '/public/plugins/jqvmap/maps/jquery.vmap.usa.js',
-        '/public/plugins/jquery-knob/jquery.knob.min.js',
-        '/public/plugins/moment/moment.min.js',
-        '/public/plugins/daterangepicker/daterangepicker.js',
-        '/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
-        '/public/plugins/summernote/summernote-bs4.min.js',
-        '/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
-        '/public/dist/js/adminlte.js',
-        '/public/dist/js/pages/dashboard.js',
-        '/public/dist/css/adminlte.min.css'
-      ],
-    },
-  },
+export default defineConfig({
   plugins: [react()],
-});
+})
