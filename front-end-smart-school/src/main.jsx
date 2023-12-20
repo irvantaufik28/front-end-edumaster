693842c8a43d84f9f384d2d93901542fa0829f0b
irvantaufik <irvantaufik28@gmail.com>
2023-12-20 11:47:40 +0700

    vite config v2

--- front-end-smart-school/src/main.jsx
+++ front-end-smart-school/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { CookiesProvider } from 'react-cookie';
 import store from '../src/store.jsx'
-
+import '/public/dist/css/adminlte.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   
 <CookiesProvider>
--- front-end-smart-school/vite.config.js
+++ front-end-smart-school/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   build: {
     outDir: 'dist', // Output directory for production build
     assetsDir: 'public', // Relative path to public assets directory
