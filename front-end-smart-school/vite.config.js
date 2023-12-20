b92c1a8d65eda5697a13e8003fe4b07f3298599b
irvantaufik <irvantaufik28@gmail.com>
2023-12-20 11:53:22 +0700

    vite config v4

--- front-end-smart-school/vite.config.js
+++ front-end-smart-school/vite.config.js
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   base: '/',
   build: {
+    outDir: 'dist', // Output directory for production build
+    assetsDir: 'public', // Relative path to public assets directory
+    manifest: true, // Generate manifest.json
     rollupOptions: {
-      external: ['/public/dist/js/adminlte.js', '/public/dist/js/pages/dashboard.js'],
+      external: [
+        '/public/plugins/jquery/jquery.min.js',
+        '/public/plugins/jquery-ui/jquery-ui.min.js',
+        '/public/plugins/bootstrap/js/bootstrap.bundle.min.js',
+        '/public/plugins/chart.js/Chart.min.js',
+        '/public/plugins/sparklines/sparkline.js',
+        '/public/plugins/jqvmap/jquery.vmap.min.js',
+        '/public/plugins/jqvmap/maps/jquery.vmap.usa.js',
+        '/public/plugins/jquery-knob/jquery.knob.min.js',
+        '/public/plugins/moment/moment.min.js',
+        '/public/plugins/daterangepicker/daterangepicker.js',
+        '/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+        '/public/plugins/summernote/summernote-bs4.min.js',
+        '/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+        '/public/dist/js/adminlte.js',
+        '/public/dist/js/pages/dashboard.js',
+        '/public/dist/css/adminlte.min.css'
+      ],
     },
   },
   plugins: [react()],
-})
+};
