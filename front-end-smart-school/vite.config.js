8f5eea5ba56e75848b5ba764febaade373f6d075
irvantaufik <irvantaufik28@gmail.com>
2023-12-24 18:00:59 +0700

    vercel config

--- front-end-smart-school/vite.config.js
+++ front-end-smart-school/vite.config.js
@@ -1,5 +1,15 @@
-{
-  "rewrites";  [
-      {"source": "/(.*)", "destination": "/"}
-   ]
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    rewrites: [
+      {
+        source: '/(.*)',
+        destination: '/',
+      },
+    ],
+  },
+});
