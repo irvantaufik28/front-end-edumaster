d815cad690fff9585566d1df3bfc79c43ac6fd9b
irvantaufik <irvantaufik28@gmail.com>
2023-12-25 13:21:48 +0700

    try deploy

--- api-smart-school/src/index.ts
+++ api-smart-school/src/index.ts
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello World" });
   });
 
-app.use(userRouter);
+// app.use(userRouter);
 app.use(errorMiddleware);
 
 export default app
