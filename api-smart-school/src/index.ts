3a659e44e9618cbb63544db290bdd8bc007ebe52
irvantaufik <irvantaufik28@gmail.com>
2023-12-25 12:57:54 +0700

    try deploy

--- api-smart-school/src/index.ts
+++ api-smart-school/src/index.ts
@@ -1,48 +1,62 @@
+// import express from "express";
+// import { errorMiddleware } from "./middleware/error-middleware";
+// import { userRouter } from "./routes/api";
+// import cors from "cors"
+
+// import UserRepository from "./repository/userRepository";
+// import ClassroomService from "./service/classroomService";
+// import ClassMajorService from "./service/classMajorService";
+// import StudentService from "./service/studentService";
+// import StaffService from "./service/staffService";
+// import { logger } from "./application/logging";
+
+// const classroomUC = new ClassroomService(new UserRepository())
+// const classMajorUC = new ClassMajorService()
+// const studentUC = new StudentService()
+// const staffUC = new StaffService()
+
+// export const app = express();
+
+
+// app.use(express.json());
+// app.use((req: any, res: any, next: any) => {
+//     req.classroomUC = classroomUC,
+//     req.classMajorUC = classMajorUC,
+//     req.studentUC = studentUC,
+//     req.staffUC = staffUC
+
+//     next()
+// })
+
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "Hello World" });
+//   });
+
+
+// app.use(cors());
+
+// app.use(userRouter);
+// app.use(errorMiddleware);
+
+
+// app.listen(process.env.PORT, () => {
+//   logger.info(`App start at ${process.env.HOST}:${process.env.PORT}` );
+// });
+
+
+// export default app
+
 import express from "express";
-import { errorMiddleware } from "./middleware/error-middleware";
-import { userRouter } from "./routes/api";
-import cors from "cors"
 
-import UserRepository from "./repository/userRepository";
-import ClassroomService from "./service/classroomService";
-import ClassMajorService from "./service/classMajorService";
-import StudentService from "./service/studentService";
-import StaffService from "./service/staffService";
-import { logger } from "./application/logging";
-
-const classroomUC = new ClassroomService(new UserRepository())
-const classMajorUC = new ClassMajorService()
-const studentUC = new StudentService()
-const staffUC = new StaffService()
-
-export const app = express();
-
-
+const app = express();
 app.use(express.json());
-app.use((req: any, res: any, next: any) => {
-    req.classroomUC = classroomUC,
-    req.classMajorUC = classMajorUC,
-    req.studentUC = studentUC,
-    req.staffUC = staffUC
 
-    next()
-})
-
-
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello World" });
-  });
-
-
-app.use(cors());
-
-app.use(userRouter);
-app.use(errorMiddleware);
-
-
-app.listen(process.env.PORT, () => {
-  logger.info(`App start at ${process.env.HOST}:${process.env.PORT}` );
+app.get("/post", (req, res) => {
+  res.status(200).json({ message: "post routes" });
 });
 
-
-export default app
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+export default app;
