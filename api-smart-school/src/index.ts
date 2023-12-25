d4ffc230d52095d63451196e2d25e64e9423f320
irvantaufik <irvantaufik28@gmail.com>
2023-12-25 13:00:21 +0700

    try deploy

--- api-smart-school/src/index.ts
+++ api-smart-school/src/index.ts
@@ -1,43 +1,43 @@
-// import express from "express";
-// import { errorMiddleware } from "./middleware/error-middleware";
-// import { userRouter } from "./routes/api";
-// import cors from "cors"
+import express from "express";
+import { errorMiddleware } from "./middleware/error-middleware";
+import { userRouter } from "./routes/api";
+import cors from "cors"
 
-// import UserRepository from "./repository/userRepository";
-// import ClassroomService from "./service/classroomService";
-// import ClassMajorService from "./service/classMajorService";
-// import StudentService from "./service/studentService";
-// import StaffService from "./service/staffService";
-// import { logger } from "./application/logging";
+import UserRepository from "./repository/userRepository";
+import ClassroomService from "./service/classroomService";
+import ClassMajorService from "./service/classMajorService";
+import StudentService from "./service/studentService";
+import StaffService from "./service/staffService";
+import { logger } from "./application/logging";
 
-// const classroomUC = new ClassroomService(new UserRepository())
-// const classMajorUC = new ClassMajorService()
-// const studentUC = new StudentService()
-// const staffUC = new StaffService()
+const classroomUC = new ClassroomService(new UserRepository())
+const classMajorUC = new ClassMajorService()
+const studentUC = new StudentService()
+const staffUC = new StaffService()
 
-// export const app = express();
-
-
-// app.use(express.json());
-// app.use((req: any, res: any, next: any) => {
-//     req.classroomUC = classroomUC,
-//     req.classMajorUC = classMajorUC,
-//     req.studentUC = studentUC,
-//     req.staffUC = staffUC
-
-//     next()
-// })
+export const app = express();
 
 
-// app.get("/", (req, res) => {
-//     res.status(200).json({ message: "Hello World" });
-//   });
+app.use(express.json());
+app.use((req: any, res: any, next: any) => {
+    req.classroomUC = classroomUC,
+    req.classMajorUC = classMajorUC,
+    req.studentUC = studentUC,
+    req.staffUC = staffUC
+
+    next()
+})
 
 
-// app.use(cors());
 
-// app.use(userRouter);
-// app.use(errorMiddleware);
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World" });
+  });
+
+app.use(userRouter);
+app.use(errorMiddleware);
 
 
 // app.listen(process.env.PORT, () => {
@@ -45,18 +45,18 @@
 // });
 
 
-// export default app
+export default app
 
-import express from "express";
+// import express from "express";
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-app.get("/post", (req, res) => {
-  res.status(200).json({ message: "post routes" });
-});
+// app.get("/post", (req, res) => {
+//   res.status(200).json({ message: "post routes" });
+// });
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
-});
-export default app;
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello World" });
+// });
+// export default app;
