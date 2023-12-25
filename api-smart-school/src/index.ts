2949114163c00d49a6f40d428d4769df784afdb0
irvantaufik <irvantaufik28@gmail.com>
2023-12-25 13:11:54 +0700

    try deploy

--- api-smart-school/src/index.ts
+++ api-smart-school/src/index.ts
@@ -1,32 +1,32 @@
 import express from "express";
-import { errorMiddleware } from "./middleware/error-middleware";
-import { userRouter } from "./routes/api";
-import cors from "cors"
+// import { errorMiddleware } from "./middleware/error-middleware";
+// import { userRouter } from "./routes/api";
+// import cors from "cors"
 
-import UserRepository from "./repository/userRepository";
-import ClassroomService from "./service/classroomService";
-import ClassMajorService from "./service/classMajorService";
-import StudentService from "./service/studentService";
-import StaffService from "./service/staffService";
-import { logger } from "./application/logging";
+// import UserRepository from "./repository/userRepository";
+// import ClassroomService from "./service/classroomService";
+// import ClassMajorService from "./service/classMajorService";
+// import StudentService from "./service/studentService";
+// import StaffService from "./service/staffService";
+// import { logger } from "./application/logging";
 
-const classroomUC = new ClassroomService(new UserRepository())
-const classMajorUC = new ClassMajorService()
-const studentUC = new StudentService()
-const staffUC = new StaffService()
+// const classroomUC = new ClassroomService(new UserRepository())
+// const classMajorUC = new ClassMajorService()
+// const studentUC = new StudentService()
+// const staffUC = new StaffService()
 
 export const app = express();
 
 
 app.use(express.json());
-app.use((req: any, res: any, next: any) => {
-    req.classroomUC = classroomUC,
-    req.classMajorUC = classMajorUC,
-    req.studentUC = studentUC,
-    req.staffUC = staffUC
+// app.use((req: any, res: any, next: any) => {
+//     req.classroomUC = classroomUC,
+//     req.classMajorUC = classMajorUC,
+//     req.studentUC = studentUC,
+//     req.staffUC = staffUC
 
-    next()
-})
+//     next()
+// })
 
 
 
