e03e8fd86649ac0be80dd96f20a34b74648ad1e4
irvantaufik <irvantaufik28@gmail.com>
2023-12-26 09:49:00 +0700

    try deploy vercel

--- api-smart-school/src/index.ts
+++ api-smart-school/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { errorMiddleware } from "./middleware/error-middleware";
-import  router  from "./routes/api";
+// import { errorMiddleware } from "./middleware/error-middleware";
+// import router from "./routes/api";
 import cors from "cors"
 import { logger } from "./application/logging";
-
+// import { VercelRequest, VercelResponse } from '@vercel/node';
+import { prismaClient } from "./application/database";
 export const app = express();
 
 app.use(express.json());
@@ -17,8 +18,22 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
 
-app.use("/api/v1", router);
-app.use(errorMiddleware);
+app.get("/test", async (req, res) => {
+  try {
+    const student = await prismaClient.student.findMany();
+
+    res.status(200).json({
+      data: student,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// app.use("/api/v1", router);
+// app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 4000;
 
