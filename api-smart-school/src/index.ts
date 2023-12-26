417b42bd50ebe00d65136e516f3028d2fbc4b2f2
irvantaufik <irvantaufik28@gmail.com>
2023-12-26 10:02:37 +0700

    rollback

--- api-smart-school/src/index.ts
+++ api-smart-school/src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
-// import { errorMiddleware } from "./middleware/error-middleware";
-// import router from "./routes/api";
+import { errorMiddleware } from "./middleware/error-middleware";
+import  router  from "./routes/api";
 import cors from "cors"
 import { logger } from "./application/logging";
-// import { VercelRequest, VercelResponse } from '@vercel/node';
-import { prismaClient } from "./application/database";
+
 export const app = express();
 
 app.use(express.json());
@@ -18,22 +17,8 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
 
-app.get("/test", async (req, res) => {
-  try {
-    const student = await prismaClient.student.findMany();
-
-    res.status(200).json({
-      data: student,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-// app.use("/api/v1", router);
-// app.use(errorMiddleware);
+app.use("/api/v1", router);
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 4000;
 
@@ -42,4 +27,4 @@ app.listen(PORT, () => {
   logger.info(`App start at ${process.env.HOST}:${process.env.PORT}`);
 });
 
-export default app
+
